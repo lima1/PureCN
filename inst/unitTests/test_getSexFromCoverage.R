c7c7f973e64e001bb073c032b8af00edae402782
Markus Riester <markus.riester@novartis.com>
2016-04-15 13:38:23 +0000

    tests for runAbsolute without VCF
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PureCN@116350 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_getSexFromCoverage.R
@@ -0,0 +1,22 @@
+test_getSexFromCoverage <- function() {
+    gatk.tumor.file <- system.file("extdata", "example_tumor.txt", package="PureCN")
+    coverage <- readCoverageGatk(gatk.tumor.file)
+    sex <- getSexFromCoverage(coverage, verbose=FALSE)
+    checkTrue(is.na(sex))
+    sex <- getSexFromCoverage(gatk.tumor.file, verbose=FALSE)
+    checkTrue(is.na(sex))
+
+    chr22 <- coverage[which(coverage$chr=="chr22"),]
+    chrX <- chr22
+    chrX$chr <- "chrX"
+    chrY <- chr22
+    chrY$chr <- "chrY"
+    coverage.sex <- do.call(rbind, list(coverage, chrX, chrY))
+    sex <- getSexFromCoverage(coverage.sex)
+    checkIdentical(sex, "M")
+
+    chrY$average.coverage <- chrY$average.coverage/50
+    coverage.sex <- do.call(rbind, list(coverage, chrX, chrY))
+    sex <- getSexFromCoverage(coverage.sex)
+    checkIdentical(sex, "F")
+}    
---
+++ inst/unitTests/test_runAbsoluteCN.R
@@ -0,0 +1,18 @@
+test_runAbsoluteCN <- function() {
+    gatk.normal.file <- system.file("extdata", "example_normal.txt", 
+        package = "PureCN")
+    gatk.tumor.file <- system.file("extdata", "example_tumor.txt", 
+        package = "PureCN")
+    vcf.file <- system.file("extdata", "example_vcf.vcf", package = "PureCN")
+    gc.gene.file <- system.file("extdata", "example_gc.gene.file.txt", 
+        package = "PureCN")
+    data(purecn.example.output)
+
+    # run without a VCF
+    ret <-runAbsoluteCN(gatk.normal.file=gatk.normal.file, 
+        gatk.tumor.file=gatk.tumor.file, 
+        candidates=purecn.example.output$candidates, 
+        max.candidate.solutions=2)
+
+    checkEqualsNumeric(ret$results[[1]]$purity, 0.65, tolerance=0.1)
+}    
