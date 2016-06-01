a7b8320c5b6e9089ef4939763dbfd6c5c388ded0
Markus Riester <markus.riester@novartis.com>
2016-06-01 22:22:08 +0000

    Added more tests for createCurationFile.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PureCN@118086 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_createCurationFile.R
+++ inst/unitTests/test_createCurationFile.R
@@ -8,4 +8,20 @@ test_createCurationFile <- function() {
     checkTrue(!ret$Curated)
     checkTrue(ret$Flagged)
     checkEquals(purecn.example.output$input$sampleid, as.character(ret$Sampleid))
+
+    retx <- readCurationFile(file.rds)
+    checkEqualsNumeric( purecn.example.output$results[[1]]$purity , retx$results[[1]]$purity)
+    checkEqualsNumeric( purecn.example.output$results[[1]]$ploidy , retx$results[[1]]$ploidy, tolerance=0.1)
+
+    retx <- readCurationFile(file.rds, min.ploidy=2)
+    checkEqualsNumeric( purecn.example.output$results[[2]]$purity , retx$results[[1]]$purity)
+    checkEqualsNumeric( purecn.example.output$results[[2]]$ploidy , retx$results[[1]]$ploidy, tolerance=0.1)
+
+    retx <- readCurationFile(file.rds, max.ploidy=2)
+    checkEquals( rep(TRUE, length(retx$results)) , sapply(retx$results, function(x) x$ploidy) < 2 )
+
+    retx <- readCurationFile(file.rds, report.best.only=TRUE)
+    checkEqualsNumeric( purecn.example.output$results[[1]]$purity , retx$results[[1]]$purity)
+    checkEqualsNumeric( purecn.example.output$results[[1]]$ploidy , retx$results[[1]]$ploidy, tolerance=0.1)
+    checkEqualsNumeric( 1, length(retx$results))
 }    
