bd007019afcf4cf85e240c79b3d7b88f40453af2
Markus Riester <markus.riester@novartis.com>
2016-06-17 02:17:16 +0000

    Set seed in test.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PureCN@118701 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_autoCurateResults.R
+++ inst/unitTests/test_autoCurateResults.R
@@ -1,5 +1,6 @@
 test_autoCurateResults <- function() {
     data(purecn.example.output)
+    set.seed(123)
     ret <- autoCurateResults(purecn.example.output, bootstrap.n=100)
     checkEquals("list", class(ret))
     checkEquals(names(purecn.example.output), names(ret))
