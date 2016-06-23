8f112da50bf4096c4b43461afd35831ee4de29e3
Markus Riester <markus.riester@novartis.com>
2016-06-23 19:50:48 +0000

    Added test for getDiploid crash.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PureCN@118915 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_autoCurateResults.R
+++ inst/unitTests/test_autoCurateResults.R
@@ -5,4 +5,8 @@ test_autoCurateResults <- function() {
     checkEquals("list", class(ret))
     checkEquals(names(purecn.example.output), names(ret))
     checkEquals(2, length(ret$results) )
+    x <- purecn.example.output
+    x$results <- x$results[1]
+    ret <- autoCurateResults(x, bootstrap.n=100)
+    checkEquals(1, length(ret$results) )
 }    
