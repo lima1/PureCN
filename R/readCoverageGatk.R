6367658f53f6cebb77755739293e18bccf8d9101
Markus Riester <markus.riester@novartis.com>
2016-10-20 19:32:59 +0000

    Added check for overlapping intervals to readCoverageGatk.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PureCN@122882 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readCoverageGatk.R
+++ R/readCoverageGatk.R
@@ -29,14 +29,42 @@ file
     if (is.null(gatk$total_coverage)) gatk$total_coverage <- NA
     if (is.null(gatk$average_coverage)) gatk$average_coverage <- NA
 
-    return(data.frame(probe = gatk$Target, chr = chr, probe_start = start, 
+    tmp <- data.frame(probe = gatk$Target, chr = chr, probe_start = start, 
         probe_end = end, targeted.base = end - start + 1, sequenced.base = NA, 
         coverage = as.numeric(gatk$total_coverage), 
         average.coverage = as.numeric(gatk$average_coverage), 
-        base.with..10.coverage = NA))
+        base.with..10.coverage = NA)
+    tmp <- .checkIntervals(tmp)
+    return(tmp)
 ### A \code{data.frame} with the parsed coverage information. 
 },ex=function() {
 tumor.coverage.file <- system.file("extdata", "example_tumor.txt", 
     package="PureCN")
 coverage <- readCoverageGatk(tumor.coverage.file)
 })
+
+.checkIntervals <- function(coverage) {
+    coverageGr <- GRanges(coverage$chr, IRanges(start=coverage$probe_start,
+        end=coverage$probe_end))
+    ov <- findOverlaps(coverageGr, coverageGr)
+    dups <- duplicated(queryHits(ov))
+    
+    if (sum(dups)) {
+        dupLines <- subjectHits(ov)[dups]
+        wOv <- width(pintersect(coverageGr[queryHits(ov)], coverageGr[subjectHits(ov)]))
+        singleDups <- which(wOv[dups]==1)
+        sumSingle <- sum(wOv[dups]==1)
+        tmpMsg <- ""
+        if (sumSingle > 0) {
+            tmpMsg <- paste(sumSingle, "intervals overlap by just 1bp.")
+        }    
+        warning("Found ", sum(dups), " overlapping intervals, starting ",
+            "at line ", dupLines[1], ".\nKeeping them",
+            " for now, but you should fix the interval file. ", 
+            "Future PureCN versions might stop here. ",
+            tmpMsg)
+        #coverage <- coverage[-dupLines,]
+    }
+    coverage     
+}
+    
--- inst/unitTests/test_readCoverageGatk.R
+++ inst/unitTests/test_readCoverageGatk.R
@@ -3,4 +3,9 @@ test_readCoverageGatk <- function() {
     coverage <- readCoverageGatk(tumor.coverage.file)
     checkEquals(nrow(coverage), 10049)
     checkIdentical(as.character(coverage$chr[1]), "chr1")
+
+    tumor.overlapping.coverage.file <- system.file("extdata", "test_coverage_overlapping_intervals.txt", package="PureCN")
+    coverage <- readCoverageGatk(tumor.overlapping.coverage.file)
+    checkEquals(5, nrow(coverage))
+    checkEqualsNumeric(c(1216042, 1216045, 1216606, 1216791, 1216991), coverage$probe_start)
 }    
