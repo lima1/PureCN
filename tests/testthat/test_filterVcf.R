73f0ac81475ca7184cb772c983133d0b072790a9
lima1 <mriester@gmx.de>
2018-03-11 16:30:33 -0400

    Added test for uncovered part in .addDbField

--- tests/testthat/test_filterVcf.R
+++ tests/testthat/test_filterVcf.R
@@ -34,8 +34,22 @@ test_that("stats.file filtering works", {
 })
 
 test_that("M2 VCF with POP_AF flag is annotated with DB flag", {
+    # first check that the POP_AF field is parsed
     vcf.m2.file <- system.file("extdata", "example_mutect2.vcf.gz", package = "PureCN")
     vcf.m2 <- PureCN:::.readAndCheckVcf(vcf.m2.file, "hg38")
     expect_equal(c(TRUE, rep(FALSE, 10)), info(vcf.m2)$DB)
     expect_equal(unlist(info(vcf.m2)$POP_AF>0.001), info(vcf.m2)$DB)
+
+    # testing dbSNP annotation only (rs* variant ids)
+    # first create new VCF without the DB and POP_AF fields
+    info(vcf.m2)$POP_AF <- NULL
+    info(vcf.m2)$DB <- NULL
+    info(header(vcf.m2)) <-  info(header(vcf.m2))[-match(c("DB", "POP_AF"), rownames(info(header(vcf.m2)))),]
+    output.file <- tempfile(fileext = ".vcf")
+    writeVcf(vcf.m2, file = output.file)
+    # now parse
+    vcf.m2 <- PureCN:::.readAndCheckVcf(output.file, "hg38")
+    file.remove(output.file)
+    expect_equal(c(TRUE, rep(FALSE, 10)), info(vcf.m2)$DB)
+    expect_equal(grepl("rs", names(vcf.m2)), info(vcf.m2)$DB)
 })
