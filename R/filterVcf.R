4b10b9d30cf6dbee964fb037aa449d934be0d7e8
lima1 <mriester@gmx.de>
2018-10-11 19:16:32 -0400

    Forgot one instance of example_vcf.

--- R/filterVcf.R
+++ R/filterVcf.R
@@ -52,7 +52,7 @@
 #' 
 #' # This function is typically only called by runAbsolute via 
 #' # fun.filterVcf and args.filterVcf.
-#' vcf.file <- system.file("extdata", "example_vcf.vcf.gz", package="PureCN")
+#' vcf.file <- system.file("extdata", "example.vcf.gz", package="PureCN")
 #' vcf <- readVcf(vcf.file, "hg19")
 #' vcf.filtered <- filterVcfBasic(vcf)        
 #' 
