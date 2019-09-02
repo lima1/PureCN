4f3031e406913565fb1c95baff4f49e1fb7fa929
lima1 <mriester@gmx.de>
2019-09-02 14:51:45 -0400

    Added support for GATK3-style log-ratio files which are easier to generate and more similar to our coverage files than the GATK4 format.

--- R/readLogRatioFile.R
+++ R/readLogRatioFile.R
@@ -6,6 +6,9 @@
 #' @param file Log2 coverage file.
 #' @param format File format. If missing, derived from the file 
 #' extension. Currently GATK4 DenoiseReadCounts format supported.
+#' A simple GATK3-style format, two columns with coordinates
+#' as string in format chr:start-stop in first and log2-ratio
+#' in second is also supported.
 #' @param zero Start position is 0-based. Default is \code{FALSE}
 #' for GATK, \code{TRUE} for BED file based intervals.
 #' @return A \code{GRange} with the log2 ratio.
@@ -17,9 +20,24 @@
 #' logratio <- readLogRatioFile(logratio.file)
 #' 
 #' @export readLogRatioFile
-readLogRatioFile <- function(file, format, zero=NULL) {
-    if (missing(format)) format <- "GATK4"
+readLogRatioFile <- function(file, format, zero = NULL) {
+    if (missing(format)) format <- .getLogRatioFormat(file)
+    if (format == "GATK3") return(.readLogRatioFileGATK3(file, zero = FALSE))
     if (format == "GATK4") return(.readLogRatioFileGATK4(file, zero = FALSE))
+}
+
+.getLogRatioFormat <- function(file) {
+     header <- scan(file, what = character(), sep = "\n", nmax = 1, quiet = TRUE)
+     format <- "GATK4"
+     if (grepl("^Target", header)[1]) return("GATK3")
+     format
+}
+
+.readLogRatioFileGATK3 <- function(file, zero=FALSE) {
+    x <- fread(file, data.table = FALSE)
+    gr <- GRanges(x[,1])
+    gr$log.ratio <- x[,2]
+    gr
 }
 
 .readLogRatioFileGATK4 <- function(file, zero = FALSE) {
--- tests/testthat/test_readLogRatioFile.R
+++ tests/testthat/test_readLogRatioFile.R
@@ -7,4 +7,9 @@ test_that("Example data matches", {
     expect_equal(21, length(logratio))
     expect_equal(0.109473, logratio$log.ratio[1], tolerance = .00001)
     expect_equal(-0.185664, logratio$log.ratio[21], tolerance = .00001)
+    logratio.file2 <- system.file("extdata", "example_logratio.txt.gz", 
+        package = "PureCN")
+    logratio2 <- readLogRatioFile(logratio.file2)
+    expect_equal(as.character(logratio), as.character(logratio2))
+    expect_equal(logratio$log.ratio, logratio2$log.ratio)
 })
